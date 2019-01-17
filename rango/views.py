ce3f6ebb858f8914210cbd6adcbfa057f061ba42
Glory Asuquo <2361383E@student.gla.ac.uk>
2019-01-17 21:21:28 +0000

    Chapter 3 Exercise completion- linking of views: index page and about page, mapping of urls.

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,9 @@ from django.http import HttpResponse
 
 # The code shows one of the individual part of the view called index, this is represented by a function.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('Rango says hey there partner!' + '<br/> <a href="/rango/about/">About Page</a>')
+
+
+# About Method
+def about(request):
+    return HttpResponse( 'Rango says hey there partner!' + '<br/> <a href="/rango/index/">Index Page</a>')
