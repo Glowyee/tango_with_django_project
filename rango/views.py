965a12e748e94b1bc594815c78eeef155320942b
Glory Asuquo <2361383E@student.gla.ac.uk>
2019-01-18 17:52:31 +0000

    Follow up of Chapter four.

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,15 @@ from django.http import HttpResponse
 
 # The code shows one of the individual part of the view called index, this is represented by a function.
 def index(request):
-    return HttpResponse('Rango says hey there partner!' + '<br/> <a href="/rango/about/">About Page</a>')
+    # construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{boldmessage}} in the templates!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcakes!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the first template we wish to use
+    # and the second is a template context- python dictionary.
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 # About Method
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# for easy automatic location of the template directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# telling django that static directory will be uploaded to project root.y
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# telling django that media directory will be uploaded to project root.
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,7 +34,6 @@ SECRET_KEY = 'h6!_j(l2z(#vrm$i@*1@%--35as^s+8y#3qcf07!p74%y&jxv1'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,13 +70,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
@@ -80,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -100,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -114,8 +119,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# Static files data structure for locating the static files on your computer.
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+# Media Files
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
